15e563c6f8bbf097b3782c9e092c5fe4dc5e96cd
Ying Yang <yingyang@uw.edu>
2017-05-18 23:36:18 -0700

    answer exercise 3

--- exercise-3/exercise.js
+++ exercise-3/exercise.js
@@ -8,3 +8,18 @@
    instead of the number. For numbers which are multiples of both three and five,
    print "FizzBuzz".
 */
+
+for(var i = 1; i<=100; i++){
+    if(i%3==0 && i%5==0){
+        console.log("FizzBuzz");
+    }
+    else if(i%3==0){
+        console.log("Fizz");
+    }
+    else if(i%5==0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i)
+    }
+}
